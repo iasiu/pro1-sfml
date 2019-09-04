9cd039037e3d3021ada54165feba9d2943dc847b
Jan Lewandowski <j.lewandowski.5@stud.elka.pw.edu.pl>
2019-09-04 19:43:58 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "JUMP");
+
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            {
+                return 0;
+                window.close();
+            }
+        }
+
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
