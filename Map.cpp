3b3c7b80284e9af072a21815d0e3bb571911bfa6
Jan Lewandowski <iasiu@icloud.com>
2019-09-07 21:50:33 +0200

    Koniec roboty na dziś, perlin śmiga

--- Map.cpp
+++ Map.cpp
@@ -21,58 +21,57 @@ void Map::load(){
         {
             float smoothness = 15;
             this->mapSquares[i][j] = 7 + (SimplexNoise::noise(i/smoothness, j/smoothness) * 6);
-            std::cout<<mapSquares[i][j]<<std::endl;
             
             this->square[i][j].setSize(sf::Vector2f(GRID, GRID));
             this->square[i][j].setPosition(i*GRID, j*GRID);
             
             switch (mapSquares [i][j]) {
                 case 1:
-                    this->square[i][j].setFillColor(sf::Color::Green);
+                    this->square[i][j].setFillColor(sf::Color(0,255,0,255));
                     break;
                     
                 case 2:
-                    this->square[i][j].setFillColor(sf::Color::Green);
+                    this->square[i][j].setFillColor(sf::Color(0,204,0,255));
                     break;
                     
                 case 3:
-                    this->square[i][j].setFillColor(sf::Color::Green);
+                    this->square[i][j].setFillColor(sf::Color(0,153,0,255));
                     break;
                     
                 case 4:
-                    this->square[i][j].setFillColor(sf::Color::Green);
+                    this->square[i][j].setFillColor(sf::Color(255,255,0,255));
                     break;
                     
                 case 5:
-                    this->square[i][j].setFillColor(sf::Color::Green);
+                    this->square[i][j].setFillColor(sf::Color(255,178,102,255));
                     break;
                     
                 case 6:
-                    this->square[i][j].setFillColor(sf::Color::Green);
+                    this->square[i][j].setFillColor(sf::Color(255,128,0,255));
                     break;
                     
                 case 7:
-                    this->square[i][j].setFillColor(sf::Color::Yellow);
+                    this->square[i][j].setFillColor(sf::Color(255,102,102,255));
                     break;
                     
                 case 8:
-                    this->square[i][j].setFillColor(sf::Color::Yellow);
+                    this->square[i][j].setFillColor(sf::Color(255,51,51,255));
                     break;
                     
                 case 9:
-                    this->square[i][j].setFillColor(sf::Color::Blue);
+                    this->square[i][j].setFillColor(sf::Color(255,0,0,255));
                     break;
                     
                 case 10:
-                    this->square[i][j].setFillColor(sf::Color::Blue);
+                    this->square[i][j].setFillColor(sf::Color(127,0,255,255));
                     break;
                     
                 case 11:
-                    this->square[i][j].setFillColor(sf::Color::Cyan);
+                    this->square[i][j].setFillColor(sf::Color(102,0,204,255));
                     break;
                     
                 case 12:
-                    this->square[i][j].setFillColor(sf::Color::Cyan);
+                    this->square[i][j].setFillColor(sf::Color(76,0,153,255));
                     break;
                     
                 default:
