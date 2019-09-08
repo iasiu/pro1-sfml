2ee7740c0e4d9ee3a81739945e63b4b756564e7a
Jan Lewandowski <iasiu@icloud.com>
2019-09-08 16:47:44 +0200

    Kolory lądu dopasowane do żądanego efektu

--- Map.cpp
+++ Map.cpp
@@ -19,59 +19,32 @@ void Map::load(){
     for(int i=0; i<100; i++)
         for(int j=0; j<100; j++)
         {
-            float smoothness = 40;
+            float smoothness = 30;
             this->mapSquares[i][j] = 7 + (SimplexNoise::noise(i/smoothness, j/smoothness) * 6);
             
             this->square[i][j].setSize(sf::Vector2f(GRID, GRID));
             this->square[i][j].setPosition(i*GRID, j*GRID);
             
             switch (mapSquares [i][j]) {
-                case 1:
-                    this->square[i][j].setFillColor(sf::Color(0,255,0,255));
-                    break;
+                case 1 ... 8:
+                {
+                    int randNum = 1 + std::rand()%8;
                     
-                case 2:
-                    this->square[i][j].setFillColor(sf::Color(0,204,0,255));
+                    if(randNum == 1 || randNum == 2 || randNum == 3)
+                        this->square[i][j].setFillColor(sf::Color(0,255,0,255));
+                    else if(randNum == 4 || randNum == 5 || randNum == 6)
+                        this->square[i][j].setFillColor(sf::Color(0,204,0,255));
+                    else if(randNum == 7 || randNum == 8)
+                        this->square[i][j].setFillColor(sf::Color(0,153,0,255));
                     break;
+                }
                     
-                case 3:
-                    this->square[i][j].setFillColor(sf::Color(0,153,0,255));
-                    break;
-                    
-                case 4:
+                case 9 ... 10:
                     this->square[i][j].setFillColor(sf::Color(255,255,0,255));
                     break;
                     
-                case 5:
-                    this->square[i][j].setFillColor(sf::Color(255,178,102,255));
-                    break;
-                    
-                case 6:
-                    this->square[i][j].setFillColor(sf::Color(255,128,0,255));
-                    break;
-                    
-                case 7:
-                    this->square[i][j].setFillColor(sf::Color(255,102,102,255));
-                    break;
-                    
-                case 8:
-                    this->square[i][j].setFillColor(sf::Color(255,51,51,255));
-                    break;
-                    
-                case 9:
-                    this->square[i][j].setFillColor(sf::Color(255,0,0,255));
-                    break;
-                    
-                case 10:
-                    this->square[i][j].setFillColor(sf::Color(127,0,255,255));
-                    break;
-                    
-                case 11:
-                    this->square[i][j].setFillColor(sf::Color(102,0,204,255));
-                    break;
-                    
-                case 12:
-                    this->square[i][j].setFillColor(sf::Color(76,0,153,255));
+                case 11 ... 12:
+                    this->square[i][j].setFillColor(sf::Color::Blue);
                     break;
                     
                 default:
@@ -119,7 +92,6 @@ void Map::checkMouseOver(float mx, float my, float cx, float cy, sf::RenderWindo
             {
                 fCol.a = 150;
                 square[i][j].setFillColor(fCol);
-                std::cout << "CONTAINS" << std::endl;
             }
             else
             {
